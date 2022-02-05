5e91694062bde528ca8a6f3dc07aca9a72bb9c27
Misbah Aamer <misbahuddin.aamer@gmail.com>
2022-02-05 18:05:04 -0500

    rangesumquery

--- Leetcode/arrays/RangeSumQuery.js
+++ Leetcode/arrays/RangeSumQuery.js
@@ -33,4 +33,40 @@ numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
 numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
 */
 
-function RangeSubQuery(nums) {}
+var NumArray = function(nums) {
+    var prefixArray = new Array(nums.length);
+   prefixArray[0] = nums[0];
+   for (var index = 1; index < prefixArray.length; index++) {
+       prefixArray[index] = prefixArray[index - 1] + nums[index];
+   }
+   this.prefixArray = prefixArray;
+   this.nums = nums;
+};
+
+/** 
+* @param {number} left 
+* @param {number} right
+* @return {number}
+*/
+NumArray.prototype.sumRange = function(i, j) {
+    return this.prefixArray[j] - this.prefixArray[i] + this.nums[i];
+};
+
+/** 
+* Your NumArray object will be instantiated and called as such:
+* var obj = new NumArray(nums)
+* var param_1 = obj.sumRange(left,right)
+*/
+
+/*
+the key idea here is:
+Sum from i to j = (sum from 0 to i) -(sum from 0 to j) + (nums[i]) 
+
+so we use a prefix sum array that will precompute and store all the sums from 0 to someIndex.
+*/
+
+/**
+* Your NumArray object will be instantiated and called as such:
+* var obj = new NumArray(nums)
+* var param_1 = obj.sumRange(i,j)
+*/
