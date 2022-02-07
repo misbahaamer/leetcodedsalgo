6a38a10b143ed40301b31dcd9a557da182b12555
Misbah Aamer <misbahuddin.aamer@gmail.com>
2022-02-06 21:37:17 -0500

    merge two lists

---
+++ Leetcode/intervals/MeetingRooms.js
@@ -0,0 +1,27 @@
+/*
+252. Meeting Rooms
+Easy
+
+1273
+
+63
+
+Add to List
+
+Share
+Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+
+ 
+
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: false
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: true
+*/
+var canAttendMeetings = function (intervals) {
+    
+};
--- Leetcode/linkedlist/MergeTwoLists.js
+++ Leetcode/linkedlist/MergeTwoLists.js
@@ -36,4 +36,27 @@ function ListNode(val, next) {
   this.next = next === undefined ? null : next;
 }
 
-var mergeTwoLists = function (list1, list2) {};
+var mergeTwoLists = function (list1, list2) {
+    let dummy = new ListNode();
+    let tail = dummy;
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            tail.next = list1;
+            list1 = list1.next;
+        }
+        else{
+            tail.next = list2;
+            list2 = list2.next;
+        }
+        tail = tail.next;
+
+    }
+    if (list1) {
+        tail.next = list1;
+    }
+    else if (list2) {
+        tail.next = list2;
+    }
+    return dummy.next;
+};
