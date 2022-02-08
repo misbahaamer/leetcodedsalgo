97d67231b649da08b2a2771e8fb7c4e74ddfa708
Misbah Aamer <misbahuddin.aamer@gmail.com>
2022-02-08 15:17:54 -0500

    unique character

--- Leetcode/strings/UniqueCharacter.js
+++ Leetcode/strings/UniqueCharacter.js
@@ -30,4 +30,18 @@ Output: -1
  * @param {string} s
  * @return {number}
  */
-var firstUniqChar = function (s) {};
+var firstUniqChar = function (s) {
+  let map = new Map();
+
+  for (i = 0; i < s.length; i++) {
+    map.set(s[i], (map.get(s[i]) || 0) + 1);
+  }
+  console.log(map);
+  for (i = 0; i < s.length; i++) {
+    if (map.has(s[i]) && map.get(s[i]) === 1) {
+      return i;
+    }
+  }
+
+  return -1;
+};
